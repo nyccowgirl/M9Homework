fa22d08f4017bad4ae03634fb283c4692d790233
nyccowgirl <nyc_cowgirl@hotmail.com>
2021-03-31 13:34:51 -0500

    refactored binary solution for findIndices

--- src/Practice.java
+++ src/Practice.java
@@ -1,45 +1,78 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Practice {
 
     public static void main(String[] args) {
-        Comparable[] array1 = {1, 3, 5, 11, 2, 8, 3, 2, 15};
-        Comparable[] array2 = {0, 1, 2, 3, 3, 3, 5, 7, 11};
+        Integer[] array1 = {1, 3, 5, 11, 2, 8, 3, 2, 15};
+        Integer[] array2 = {0, 1, 2, 3, 3, 3, 5, 7, 11};
 
-        ArrayList<Integer> indices1 = new ArrayList<>();
+        ArrayList<Integer> indices1;
         indices1 = findIndicesRecursiveSorted(array1, 3);
-        System.out.println(indices1.toString());
+        System.out.println(indices1.toString());                // unsorted: [1, 6]; sorted linear: [1]; binary: [6]
 
-        ArrayList<Integer> indices2 = new ArrayList<>();
+        ArrayList<Integer> indices2;
         indices2 = findIndicesRecursiveSorted(array2, 3);
-        System.out.println(indices2.toString());
+        System.out.println(indices2.toString());                // [3, 4, 5]
     }
 
     public static ArrayList<Integer> findIndicesRecursiveSorted(Comparable[] array, Comparable target) {
         ArrayList<Integer> result = new ArrayList<>();
-
         findIndicesRecursiveSorted(result, array, 0, array.length - 1, target);
-
         return result;
     }
 
     public static void findIndicesRecursiveSorted(ArrayList indices, Comparable[] array, int start, int end, Comparable target) {
-        if (array[start].compareTo(target) == 0) {
-            indices.add(start);
-        }
-
         // For unsorted:
-//        if (start == end) {
+//        if (start > end) {
 //            return;
 //        } else {
-//            findIndicesRecursiveSorted(temp, array, start + 1, end, target);
+//            if (array[start].compareTo(target) == 0) {
+//                indices.add(start);
+//            }
+//            findIndicesRecursiveSorted(indices, array, start + 1, end, target);
 //        }
 
         // For sorted - ends if end of list or value is greater than target:
-        if (start == end || array[start].compareTo(target) > 0) {
+        // Linear/sequential
+        if (start > end || array[start].compareTo(target) > 0) {
             return;
         } else {
+            if (array[start].compareTo(target) == 0) {
+                indices.add(start);
+            }
             findIndicesRecursiveSorted(indices, array, start + 1, end, target);
         }
+
+        // Binary
+//        if (start > end) {
+//            return;
+//        }
+//
+//        int mid = ((end - start) / 2) + start;
+//
+//        if (array[mid].compareTo(target) == 0) {
+//            indices.add(mid);
+//
+//            // check both sides for dupes
+//            int left = mid - 1;
+//            int right = mid + 1;
+//            while (array[left].compareTo(target) == 0) {
+//                indices.add(left);
+//                left--;
+//            }
+//
+//            while (array[right].compareTo(target) == 0) {
+//                indices.add(right);
+//                right++;
+//            }
+//
+//            Collections.sort(indices);
+//            return;
+//        } else if (array[mid].compareTo(target) > 0) {
+//            findIndicesRecursiveSorted(indices, array, start, mid - 1, target);
+//        } else {
+//            findIndicesRecursiveSorted(indices, array, mid + 1, end, target);
+//        }
     }
 }
